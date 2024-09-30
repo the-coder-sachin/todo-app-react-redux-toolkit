c8256ce6aa2d16f53ecc34ed770a5b1dfc8fae55
krssachinn@gmail.com <krssachinn@gmail.com>
2024-09-30 16:04:44 +0530

    learnt basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { addTodo, removeTodo } from './features/todoSlice'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [input, setInput] = useState('')
+  const dispatch = useDispatch();
+  const todos = useSelector(state => state.todos)
+  const addTodoHandle = ()=>{
+    dispatch(addTodo(input))
+    setInput('')
+  }
+  const removeTodoHandler = (id)=>{
+    dispatch(removeTodo(id))
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="flex gap-2 p-4">
+        <input
+          className="bg-slate-900"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="bg-green-500 px-2 py-1" onClick={addTodoHandle}>
+          add
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="flex flex-col p-4 gap-2">
+        {todos.map((todo) => (
+          <li key={todo.id} className="flex justify-between">
+            <h1>list of todo{todo.text}</h1>
+            <button className="bg-red-500 px-2 py-1 text-xs" onClick={()=>removeTodoHandler(todo.id)}>remove</button>
+          </li>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
 export default App
